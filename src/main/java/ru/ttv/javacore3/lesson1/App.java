0cf53d32e08c94e1ce0cd1150d6e5e847a39f191
timteplych <timteplych@mail.ru>
2018-07-04 13:10:35 +0800

    Initial commit

---
+++ src/main/java/ru/ttv/javacore3/lesson1/App.java
@@ -0,0 +1,17 @@
+package ru.ttv.javacore3.lesson1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    private static void toggleArrayElements(){
+        
+    }
+}
