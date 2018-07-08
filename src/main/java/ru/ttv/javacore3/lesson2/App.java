39ad7a9eab6c3d91766fe0335783c55475d952cf
timteplych <timteplych@mail.ru>
2018-07-08 18:29:50 +0800

    init

---
+++ src/main/java/ru/ttv/javacore3/lesson2/App.java
@@ -0,0 +1,13 @@
+package ru.ttv.javacore3.lesson2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) {
+
+
+
+    }
+}
