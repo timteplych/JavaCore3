7e652ac8d7a57bc155f5526c0294d53dc309ad56
timteplych <timteplych@mail.ru>
2018-07-09 00:11:26 +0800

    Домашнее задание по второму уроку

--- src/main/java/ru/ttv/javacore3/lesson2/App.java
+++ src/main/java/ru/ttv/javacore3/lesson2/App.java
@@ -1,13 +1,59 @@
 package ru.ttv.javacore3.lesson2;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        String url = "jdbc:sqlite:C:/sqlite/db/ttv.db";
+        // create a connection to the database
+        Connection conn = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
+            //DatabaseMetaData dbMeta = conn.getMetaData();
 
+            DBprocess dbProcess = new DBprocess(conn);
+            //dbProcess.initDB();
 
-
+            Scanner scanner = new Scanner(System.in);
+            while(true){
+                String input = scanner.nextLine();
+                if("exit".equals(input)) break;
+                String[] splittedMas = input.split("\\s");
+                if(splittedMas.length >= 2 ){
+                    if("/цена".equals(splittedMas[0])){
+                        Float cost = dbProcess.getCostByProductTitle(splittedMas[1]);
+                        if(cost == null){
+                            System.out.println("Такого товара нет");
+                        }else {
+                            System.out.println(cost);
+                        }
+                    }
+                    if("/сменитьцену".equals(splittedMas[0])){
+                        dbProcess.changeNewPrice(splittedMas[1],Float.parseFloat(splittedMas[2]));
+                    }
+                    if("/товарыпоцене".equals(splittedMas[0])){
+                        dbProcess.showProductRange(Float.parseFloat(splittedMas[1]),Float.parseFloat(splittedMas[2]));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                if(conn != null){
+                    conn.close();
+                }
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ src/main/java/ru/ttv/javacore3/lesson2/DBprocess.java
@@ -0,0 +1,76 @@
+package ru.ttv.javacore3.lesson2;
+
+import java.sql.*;
+
+public class DBprocess {
+    private Connection conn;
+
+    public DBprocess(Connection conn){
+        this.conn = conn;
+    }
+
+    public void initDB(){
+        try {
+            Statement stmt = conn.createStatement();
+            //initial query
+            stmt.execute(getInitialQuery());
+            //filling demo data
+            fillDemoData();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fillDemoData() throws SQLException {
+        String query = "INSERT INTO products (PRODID, TITLE, COST) VALUES (?,?,?)";
+        PreparedStatement pstmnt = conn.prepareStatement(query);
+        for(int i=1;i<=10000; i++){
+            pstmnt.setString(1,"id_product"+i);
+            pstmnt.setString(2,"product"+i);
+            pstmnt.setFloat(3,new Float(i*10));
+            pstmnt.executeUpdate();
+        }
+    }
+
+    private String getInitialQuery() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("CREATE TABLE IF NOT EXISTS  products (");
+        sb.append("ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,");
+        sb.append("PRODID TEXT NOT NULL,");
+        sb.append("TITLE TEXT,");
+        sb.append("COST REAL);");
+        sb.append("DELETE FROM products;");
+        return sb.toString();
+    }
+
+    public Float getCostByProductTitle(String title) throws SQLException {
+        String query = "SELECT COST FROM products WHERE TITLE = ?";
+        PreparedStatement pstmnt = conn.prepareStatement(query);
+        pstmnt.setString(1,title);
+        ResultSet rs = pstmnt.executeQuery();
+        if(rs.next()){
+            return rs.getFloat(1);
+        }
+        return null;
+    }
+
+    public void changeNewPrice(String title, Float newCost) throws SQLException {
+        String query = "UPDATE products SET COST = ? WHERE TITLE = ?";
+        PreparedStatement pstmnt = conn.prepareStatement(query);
+        pstmnt.setFloat(1,newCost);
+        pstmnt.setString(2,title);
+        pstmnt.execute();
+    }
+
+    public void showProductRange(Float startCost, Float endCost) throws SQLException {
+        String query = "SELECT PRODID, TITLE, COST FROM products WHERE COST >= ? AND COST <= ?";
+        PreparedStatement pstmnt = conn.prepareStatement(query);
+        pstmnt.setFloat(1,startCost);
+        pstmnt.setFloat(2,endCost);
+        ResultSet rs = pstmnt.executeQuery();
+        while(rs.next()){
+            //return rs.getFloat(1);
+            System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+new Float(rs.getFloat(3)));
+        }
+    }
+}
