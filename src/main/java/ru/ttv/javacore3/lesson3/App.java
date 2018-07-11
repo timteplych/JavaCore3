6b36fb729cb6eb5da6a081f166f32cee9c825c3b
timteplych <timteplych@mail.ru>
2018-07-11 20:49:01 +0800

    Домашнее задание по третьему уроку

--- src/main/java/ru/ttv/javacore3/lesson3/App.java
+++ src/main/java/ru/ttv/javacore3/lesson3/App.java
@@ -1,6 +1,8 @@
 package ru.ttv.javacore3.lesson3;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
@@ -28,6 +30,14 @@ public class App {
             System.out.println(result);
         }
 
+        //Task2
+        String[] paths = new String[5];
+        paths[0] = "C:/JavaProjects/1.txt";
+        paths[1] = "C:/JavaProjects/2.txt";
+        paths[2] = "C:/JavaProjects/3.txt";
+        paths[3] = "C:/JavaProjects/4.txt";
+        paths[4] = "C:/JavaProjects/5.txt";
+        File resultFile = myFileReader.getJoinedFile(paths);
     }
 
 
--- src/main/java/ru/ttv/javacore3/lesson3/MyFileReader.java
+++ src/main/java/ru/ttv/javacore3/lesson3/MyFileReader.java
@@ -1,15 +1,17 @@
 package ru.ttv.javacore3.lesson3;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
 
 public class MyFileReader {
 
     public byte[] getArrayFileData(String pathToFile){
         byte[] masResult = new byte[100];
-        try {
-            FileInputStream fileInputStream = new FileInputStream(pathToFile);
+        try (FileInputStream fileInputStream = new FileInputStream(pathToFile)){
+
             fileInputStream.read(masResult);
 
         } catch (FileNotFoundException e) {
@@ -17,6 +19,36 @@ public class MyFileReader {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return masResult;
+    }
+
+    public File getJoinedFile(String[] pathArray){
+        List<InputStream> arrayListOfStreams = new ArrayList<InputStream>();
+        for(String path : pathArray){
+            try {
+                arrayListOfStreams.add(new FileInputStream(path));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        Enumeration<InputStream> e = Collections.enumeration(arrayListOfStreams);
+
+
+        File outPutFile = new File("c:/JavaProjects/result12345.txt");
+        try ( SequenceInputStream sequenceInputStream = new SequenceInputStream(e);
+              FileOutputStream fileOutputStream = new FileOutputStream(outPutFile)){
+            int writeData = sequenceInputStream.read();
+            while(writeData != -1) {
+                fileOutputStream.write(writeData);
+                writeData = sequenceInputStream.read();
+            }
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        return outPutFile;
     }
 }
