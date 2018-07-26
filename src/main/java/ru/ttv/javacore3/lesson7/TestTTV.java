5c73f989e071c02b019833d73c4c65648d7d51ee
timteplych <timteplych@mail.ru>
2018-07-26 11:45:33 +0800

    Домашнее задание по седьмому уроку

--- src/main/java/ru/ttv/javacore3/lesson7/TestTTV.java
+++ src/main/java/ru/ttv/javacore3/lesson7/TestTTV.java
@@ -2,11 +2,20 @@ package ru.ttv.javacore3.lesson7;
 
 public class TestTTV {
     public static void start(Class testClass){
-
+        testProcess(testClass);
     }
 
     public static void start(String className){
+        Class testClass = null;
+        try {
+            testClass = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        testProcess(testClass);
 
+    }
+    private static void testProcess(Class testClass) {
 
     }
 }
