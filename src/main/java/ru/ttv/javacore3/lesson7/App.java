0dd779eca9d3e474ac00802796d0ea7e0ddf6d73
timteplych <timteplych@mail.ru>
2018-07-26 14:30:06 +0800

    Домашнее задание по седьмому уроку

--- src/main/java/ru/ttv/javacore3/lesson7/App.java
+++ src/main/java/ru/ttv/javacore3/lesson7/App.java
@@ -1,11 +1,8 @@
 package ru.ttv.javacore3.lesson7;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-
 public class App {
     public static void main(String[] args) {
         TestTTV.start(ReflectionTestClass.class, new ReflectionTestClass());
+        TestTTV.start("ru.ttv.javacore3.lesson7.ReflectionTestClass",new ReflectionTestClass());
     }
 }
